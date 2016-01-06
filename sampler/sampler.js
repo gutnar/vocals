1308f417e83e4d7c31efa6aa29c06584ace28c11
Gutnar Leede <gutnar@gmail.com>
2016-01-06 14:23:10 +0200

    Noise removal

--- sampler/sampler.js
+++ sampler/sampler.js
@@ -88,39 +88,57 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
         {name: 'E'},
         {name: 'I'},
         {name: 'O'},
-        {name: 'U'}
+        {name: 'U'},
+        {name: 'M'},
+        {name: 'S'}
     ];
 
     // Analyser
     var analyser;
 
-    function updateAnalysers(canvasId, loop) {
-        var canvas = document.getElementById(canvasId);
-        var canvasWidth = canvas.width;
-        var canvasHeight = canvas.height;
-        var analyserContext = canvas.getContext('2d');
+    function updateAnalysers(canvasId, loop, normalize) {
+        var i;
 
-        // analyzer draw code here
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
+        // Fourier transform
+        var fft = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(fft);
 
-        var freqByteData = new Uint8Array(analyser.frequencyBinCount);
+        // Remove noise
+        if ($scope.vocals[0].data) {
+            for (i = 0; i < fft.length; i++) {
+                if ($scope.vocals[0].data[i] > fft[i]) {
+                    fft[i] = 0;
+                } else {
+                    fft[i] -= $scope.vocals[0].data[i];
+                }
+            }
+        }
 
-        analyser.getByteFrequencyData(freqByteData);
 
         // Normalize
-        var i, max = Math.max.apply(null, freqByteData);
+        var max = Math.max.apply(null, fft);
 
-        for (i = 0; i < freqByteData.length; ++i) {
-            freqByteData[i] = freqByteData[i] / max * 255;
+        if (normalize) {
+            for (i = 0; i < fft.length; ++i) {
+                fft[i] = fft[i] / max * 255;
+
+                /*
+                 if ($scope.vocals[0].data) {
+                 if ($scope.vocals[0].data > fft[i]) {
+                 fft[i] = 0;
+                 } else {
+                 fft[i] -= $scope.vocals[0].data[i];
+                 }
+                 }
+                 */
+            }
         }
 
         // Average value
         var mean = 0;
 
-        for (i = 0; i < freqByteData.length; i++) {
-            mean += freqByteData[i];
+        for (i = 0; i < fft.length; i++) {
+            mean += fft[i];
         }
 
         mean /= i;
@@ -128,8 +146,8 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
         // Standard deviation
         var variance = 0;
 
-        for (i = 0; i < freqByteData.length; i++) {
-            variance += Math.pow(freqByteData[i] - mean, 2);
+        for (i = 0; i < fft.length; i++) {
+            variance += Math.pow(fft[i] - mean, 2);
         }
 
         variance /= i;
@@ -153,13 +171,13 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
 
                 vocal.error = 0;
 
-                for (j = 0; j < freqByteData.length; ++j) {
-                    vocal.error += Math.pow(freqByteData[j] - vocal.data[j], 2);
+                for (j = 0; j < fft.length; ++j) {
+                    vocal.error += Math.pow(fft[j] - vocal.data[j], 2);
                 }
 
                 //vocal.error = Math.round(vocal.error/(256*256*freqByteData.length)*100);
 
-                log += vocal.name + ' (' + vocal.correlation + '), ';
+                log += vocal.name + ' (' + vocal.error + '), ';
 
                 if (!best || vocal.error < best.error) {
                     best = vocal;
@@ -175,16 +193,38 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
         }
 
         if (log) {
-            console.log(log + best.name);
-
             $scope.$apply(function () {
                 best.active = true;
             });
         }
 
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
+        // Draw signal
+        var canvas = document.getElementById(canvasId);
+        var ctx = canvas.getContext('2d');
+
+        // Clear current frame
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#F6D565';
+        ctx.lineCap = 'round';
+
+        // Draw fft
+        var bars = Math.floor(canvas.width/4);
+        var inBars = Math.floor(fft.length/bars);
+
+        for (i = 0; i < bars; i++) {
+            mean = 0;
+
+            for (j = 0; j < inBars; j++) {
+               mean += fft[j + i*inBars];
+            }
+
+            mean /= inBars;
+
+            ctx.fillRect(i*(canvas.width/bars-1+1), canvas.height, canvas.width/bars-1, -mean/255*canvas.height);
+        }
+
+        /*
         var multiplier = analyser.frequencyBinCount / numBars;
 
         // Draw rectangle for each frequency bin.
@@ -199,12 +239,13 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
             analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
             analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
         }
+        */
 
         if (loop !== false) {
             window.requestAnimationFrame(updateAnalysers.bind(this, canvasId));
         }
 
-        return freqByteData;
+        return fft;
     }
 
     // Get audio stream
@@ -222,12 +263,6 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
         // Index of vocal currently being recorded
         $scope.recordingIndex = -1;
 
-        $interval(function () {
-            for (var i = 0; i < $scope.vocals.length; ++i) {
-                $scope.vocals[i].showError = $scope.vocals[i].error;
-            }
-        }, 250);
-
         // Start recording vocals
         $interval(function () {
             // Get vocal to record
@@ -236,8 +271,9 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
             // Analyse recording
             if (vocal) {
                 $timeout(function () {
-                    vocal.data = updateAnalysers('vocal-' + vocal.name, false);
+                    vocal.data = updateAnalysers('vocal-' + vocal.name, false, $scope.recordingIndex !== 0);
 
+                    /*
                     // Average value
                     vocal.mean = 0;
 
@@ -255,6 +291,7 @@ app.controller('RecordCtrl', function (media, $scope, $location, $interval, $tim
                     }
 
                     vocal.variance /= j;
+                    */
                 }, 1000);
             }
         }, 2000, $scope.vocals.length + 1);
